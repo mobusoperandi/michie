0053c3814987b6e50e8d93911c4abbd50924e5b4
Roland Fredenhagen <dev@modprog.de>
2022-07-09 08:34:45 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 
 use std::{
+    borrow::Borrow,
     collections::{BTreeMap, HashMap},
     hash::Hash,
 };
@@ -9,32 +10,32 @@ use std::{
 pub use michie_macro::memoized;
 
 pub trait MemoizationStore<I, R> {
-    fn insert(&mut self, input: I, return_value: R) -> R;
+    fn insert(&mut self, input: &I, return_value: &R);
     fn get(&self, input: &I) -> Option<R>;
 }
 
-impl<I, R> MemoizationStore<I, R> for HashMap<I, R>
+impl<I, K, R> MemoizationStore<I, R> for HashMap<K, R>
 where
-    I: Eq + Hash,
+    I: Eq + Hash + ToOwned<Owned = K>,
+    K: Borrow<I> + Eq + Hash,
     R: Clone,
 {
-    fn insert(&mut self, input: I, return_value: R) -> R {
-        HashMap::insert(self, input, return_value.clone());
-        return_value
+    fn insert(&mut self, input: &I, return_value: &R) {
+        HashMap::insert(self, input.to_owned(), return_value.clone());
     }
     fn get(&self, input: &I) -> Option<R> {
         HashMap::get(self, input).cloned()
     }
 }
 
-impl<I, R> MemoizationStore<I, R> for BTreeMap<I, R>
+impl<I,K, R> MemoizationStore<I, R> for BTreeMap<K, R>
 where
-    I: Ord,
+    I: Ord + ToOwned<Owned = K>,
+    K: Borrow<I> + Ord,
     R: Clone,
 {
-    fn insert(&mut self, input: I, return_value: R) -> R {
-        BTreeMap::insert(self, input, return_value.clone());
-        return_value
+    fn insert(&mut self, input: &I, return_value: &R) {
+        BTreeMap::insert(self, input.to_owned(), return_value.clone());
     }
     fn get(&self, input: &I) -> Option<R> {
         BTreeMap::get(self, input).cloned()
